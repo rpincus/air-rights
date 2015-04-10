22c4aaeebe11e3a812c7fc7d9fc01e265d21e62b
rpincus <rmp385@nyu.edu>
2015-04-09 21:30:36 -0400

    Draft 2
    Draft 2

--- controllers.js
+++ controllers.js
@@ -1,13 +1,9 @@
-function BuildingListController( $scope ){
-
-	];
-
 var mapApp = angular.module("graphApp",["ngRoute","appControllers", "firebase"]);
 
 var appControllers = angular.module("appControllers", []);
 
 app.controller("SampleCtrl", function($scope, $firebaseObject) {
-  var ref = new Firebase("https://rpincus.firebaseio.com/urbanism");
+  var ref = new Firebase("https://urbanism.firebaseio.com/data");
   // download the data into a local object
   $scope.data = $firebaseObject(ref);
     $scope.graphData = $firebaseArray(ref);
